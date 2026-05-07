99449ffa30cb935128da5fe970953e06c1f8e107
LuZhiChao <416694798@qq.com>
2026-05-07 16:13:10 +0800

    优化配置

--- core/config.py
+++ core/config.py
@@ -20,22 +20,24 @@ class Settings(BaseSettings):
     algorithm: str = Field(default=os.getenv("ALGORITHM", "HS256"))
     access_token_expire_minutes: int = Field(default=os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 
+    # 系统提示词路径
     system_prompt_file_path: str = Field(
         default=os.getenv("SYSTEM_PROMPT_FILE_PATH", "../prompt/system_prompt.md"))
 
     # 通义大模型配置
     base_url: str = Field(
         default=os.getenv("BASE_URL", "https://dashscope.aliyuncs.com/compatible-mode/v1"))
-    dashscope_api_key: str = Field(default=os.getenv("DASHSCOPE_API_KEY"))
-    multimodal_model_name: str = Field(default=os.getenv("MULTIMODAL_MODEL_NAME", "qwen-vl-plus"))
-    text_model_name: str = Field(default=os.getenv("TEXT_MODEL_NAME", "qwen-vl-plus"))
-    embedding_model_name: str = Field(default=os.getenv("EMBEDDING_MODEL_NAME", "qwen-vl-plus"))
+    dashscope_api_key: str = Field(default=os.getenv("DASHSCOPE_API_KEY", ""))
+    multimodal_model_name: str = Field(default=os.getenv("MULTIMODAL_MODEL_NAME", "qwen3.6-plus"))
+    text_model_name: str = Field(default=os.getenv("TEXT_MODEL_NAME", "qwen3.5-35b-a3b"))
+    embedding_model_name: str = Field(
+        default=os.getenv("EMBEDDING_MODEL_NAME", "text-embedding-v4"))
 
     # Chroma配置
     chroma_persist_directory: str = Field(
         default=os.getenv("CHROMA_PERSIST_DIRECTORY", "./db/chroma_db"))
     chroma_collection_name: str = Field(
-        default=os.getenv("CHROMA_COLLECTION_NAME", "safety_risk_knowledge"))
+        default=os.getenv("CHROMA_COLLECTION_NAME", "risk_agent_rag"))
 
     # 文本分块配置
     chunk_size: int = 500
@@ -47,21 +49,24 @@ class Settings(BaseSettings):
     # baidu_ocr_secret_key: str = Field(default=os.getenv("BAIDU_OCR_SECRET_KEY"))
 
     # 数据库配置
+    mysql_driver: str = Field(default="mysql+aiomysql")
     mysql_db_user: str = Field(default=os.getenv("MYSQL_DB_USER", "root"))
     mysql_db_password: str = Field(default=os.getenv("MYSQL_DB_PASSWORD", "123456"))
     mysql_db_host: str = Field(default=os.getenv("MYSQL_DB_HOST", "127.0.0.1"))
-    mysql_db_port: str = Field(default=os.getenv("MYSQL_DB_PORT", "3306"))
-    mysql_db_database: str = Field(default=os.getenv("MYSQL_DB_DATABASE", "safety_risk_agent"))
-    mysql_database_url: str = Field(default=os.getenv("MYSQL_DATABASE_URL",
-                                                      f"mysql+aiomysql://{mysql_db_user}:{mysql_db_password}@{mysql_db_host}:{mysql_db_port}/{mysql_db_database}"))
+    mysql_db_port: int = Field(default=os.getenv("MYSQL_DB_PORT", 3306))
+    mysql_db_database: str = Field(default=os.getenv("MYSQL_DB_DATABASE", "safety_risk"))
 
     # redis 配置
     redis_host: str = Field(default=os.getenv("REDIS_HOST", "127.0.0.1"))
     redis_port: int = Field(default=os.getenv("REDIS_PORT", 6379))
     redis_password: str = Field(default=os.getenv("REDIS_PASSWORD", ""))
 
+    @property
+    def mysql_database_url(self) -> str:
+        return f"{self.mysql_driver}://{self.mysql_db_user}:{self.mysql_db_password}@{self.mysql_db_host}:{self.mysql_db_port}/{self.mysql_db_database}"
+
     model_config = SettingsConfigDict(
-        case_sensitive=False,  # 不区分大小写（核心作用）
+        case_sensitive=False,  # 不区分大小写
         env_file=".env",  # 自动加载 .env 文件
         env_ignore_empty=True  # 忽略空环境变量
     )
